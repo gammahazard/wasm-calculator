05fbf67985b0da4a563663beba704086546ce6d5
gammahazard <dhaliswag777@gmail.com>
2025-11-22 01:35:45 -0500

    add sqrt and power

--- build.sh
+++ build.sh
@@ -7,7 +7,7 @@ mkdir -p public
 # -I./cpp tells the compiler to look inside the cpp folder for header files
 emcc cpp/wasm_bridge.cpp -o public/calculator.js \
   -I./cpp \
-  -s EXPORTED_FUNCTIONS='["_api_add", "_api_subtract", "_api_multiply", "_api_divide"]' \
+  -s EXPORTED_FUNCTIONS='["_api_add", "_api_subtract", "_api_multiply", "_api_divide", "_api_sin", "_api_cos", "_api_tan", "_api_sqrt", "_api_pow"]' \
   -s EXPORTED_RUNTIME_METHODS='["ccall", "cwrap"]' \
   -s WASM=1
 
